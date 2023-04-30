84103cd6ab9e5b98fb0a8cf2d86b24b87cdb8aca
ashi-gaur <119112198+ashi-gaur@users.noreply.github.com>
2023-04-30 20:09:47 +0530

    Add files via upload

---
+++ powerfulInteger.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            int n=Integer.parseInt(in.readLine().trim());
+            int a[][]=new int[n][2];
+            for(int i=0;i<n;i++){
+                String s[]=in.readLine().trim().split(" ");
+                a[i][0]=Integer.parseInt(s[0]);
+                a[i][1]=Integer.parseInt(s[1]);
+            }
+            int k=Integer.parseInt(in.readLine().trim());
+            Solution ob=new Solution();
+            out.println(ob.powerfullInteger(n,a,k));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    public static int powerfullInteger(int n,int interval[][],int k)
+    {
+//         int mx = Integer.MIN_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             mx = Math.max(mx, interval[i][1]);
+//         }
+//         int[] v = new int[mx + 3];
+//         for (int i = 0; i < n; i++) {
+//             a[interval[i][0]] += 1;
+//             a[interval[i][1] + 1] -= 1;
+//         }
+//         int ans = -1;
+//         for (int i = 1; i < max + 3; i++) {
+//             a[i] = a[i - 1] + v[i];
+//             if (a[i] >= k) {
+//                 ans = i;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,interval[i][1]);
+        }
+        
+        int[] a=new int[max+3];
+        
+        for(int i=0;i<n;i++){
+            a[interval[i][0]]+=1;
+            a[interval[i][1]+1]-=1;
+        }
+        
+        int ans=-1;
+        
+        for(int i=1;i<max+3;i++){
+            a[i]=a[i-1]+a[i];
+            if(a[i]>=k){
+                ans=i;
+            }
+        }
+        return ans;
+    }
+}
