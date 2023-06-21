7f2c26f012841ed723e1b83eab86e692b5d48aa0
ashi-gaur <119112198+ashi-gaur@users.noreply.github.com>
2023-06-21 18:15:42 +0530

    Create reverseCoding.java

---
+++ reverseCoding.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(S[0]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfNaturals(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        long m=1000000007;
+        long sum=(long)n*(long)(n+1);
+        sum/=2;
+        sum%=m;
+        return (int)sum;
+    }
+};
