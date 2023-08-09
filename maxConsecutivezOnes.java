1a3c661d7b4ea779af4a34ef51942d0309c51f21
ashi-gaur <119112198+ashi-gaur@users.noreply.github.com>
2023-08-09 20:08:06 +0530

    Create maxConsecutivezOnes.java

---
+++ maxConsecutivezOnes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+         int start=0,end=0,zero=0;
+         while(end<nums.length){
+             if(nums[end]==0){ zero++; }
+             end++;
+             if(zero>k){
+                 if(nums[start]==0){
+                     zero--;
+                 }
+                 start++;
+             }
+         }
+         return end-start;
+    }
+}
